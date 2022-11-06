d59c1ea7d174ad1f66390a3ca1e3396d3587ca2f
neveah <19830717@163.com>
2022-11-06 13:40:00 +0800

    fjh

--- src/main/resources/static/pages/answerQuestionJs/login.js
+++ src/main/resources/static/pages/answerQuestionJs/login.js
@@ -1,54 +1,57 @@
-var UserName = document.getElementById("UserName");
-var Password = document.getElementById("Password");
-var LoginButton = document.getElementById("LoginButton");
-var UserNameText = '';
+document.addEventListener('DOMContentLoaded', function (event) {
+    document.addEventListener('selectstart', function (event) {
+        event.preventDefault();
+    })
+    document.addEventListener('contextmenu', function (event) {
+        event.preventDefault();
+    })
+    var random_box = document.querySelector('.random');
+    var btn = document.querySelector('.reset');
+    var wirte = document.querySelector('.write');
 
-// isLoginFun();
-//登录
-function login() {
-    if (!UserName.value) {
-        alert("请先输入用户名");
-        UserName.focus();
-        return;
-    }
-    if(!Password.value) {
-        alert("请输入密码");
-        Password.focus();
-        return;
+    function random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    UserNameText = $("#UserName").val();
-    var PasswordTest = $("#Password").val();
-
-    var da = {
-        "username":UserNameText,
-        "password":PasswordTest
-    };
-    commonAjaxPost(true, "/admin/userLogin", da, loginSuccess)
-}
-
-//登录成功回调
-function loginSuccess(result){
-    if (result.code == '666') {
-        result.data = result.data[0]
-        layer.msg(result.message, {icon:1});
-        setCookie('isLogin','1');
-        setCookie('userId',result.data.id);
-        setCookie('userName',UserNameText);
-        setCookie('power',result.data.role);
-        setCookie('modelId',result.data.modelId)
-
-        window.location.href = "myQuestionnaires.html"
-    }else{
-        //layer.msg("此用户不存在",{icon:2});
-        $('#errorMsgBox').show()
-        $('#errorMsgBox').text(result.message)
-    }
-}
-
-//回车事件
-$(document).keydown(function (event) {
-    if (event.keyCode == 13) {
-        login();
-    }
-});
+    btn.addEventListener('click', function () {
+        btn.style.backgroundColor = '#fff';
+        window.setTimeout(function (event) {
+            btn.style.backgroundColor = 'rgb(255, 224, 146)';
+        }, 50)
+        var randoms = random(1000, 9999);
+        console.log(randoms);
+        random_box.innerHTML = randoms;
+    })
+})
+$(function () {
+    $('.change-register-button').on('click', function () {
+        $('.login').animate(
+            {
+                'left': '240px'
+            }, 400, function () {
+                $('.login').css({
+                    'display': 'none',
+                    'left': '60px'
+                })
+                $('.change-register-box').css('display', 'none')
+                $('.register').css('display', 'block')
+                $('.change-login-box').css('display', 'block')
+            }
+        )
+    })
+    $('.change-login-button').on('click', function () {
+        $('.register').animate(
+            {
+                'right': '240px'
+            }, 400, function () {
+                $('.register').css({
+                    'display': 'none',
+                    'right': '60px'
+                })
+                $('.change-login-box').css('display', 'none')
+                $('.login').css('display', 'block')
+                $('.change-register-box').css('display', 'block')
+            }
+        )
+    })
+})
