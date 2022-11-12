291fc2845a34d0d9997bc5ddace11cc15d02d022
2003fjh <neveah19830717@163.com>
2022-11-12 22:52:15 +0800

    答者管理

---
+++ src/main/resources/static/pages/answerQuestionJs/respondentmanage.js
@@ -0,0 +1,198 @@
+/**
+ * Created by Amy on 2018/8/9.
+ */
+$(function () {
+    isLoginFun();
+    header();
+    $("#ctl01_lblUserName").text(getCookie('userName'));
+    var oTable = new TableInit();
+    oTable.Init();
+});
+
+//回车事件
+$(document).keydown(function (event) {
+    if (event.keyCode == 13) {
+        getUserList();
+    }
+});
+
+$('#userManager').on("keydown", function (event) {
+    var keyCode = event.keyCode || event.which;
+    if (keyCode == "13") {
+        //console.log("1111")
+        event.preventDefault();
+    }
+});
+
+function getUserList() {
+    $("#userTable").bootstrapTable('refresh');//刷新url
+}
+
+function TableInit() {
+    var oTableInit = new Object();
+    //初始化Table
+    oTableInit.Init = function () {
+        $('#userTable').bootstrapTable({
+            url: httpRequestUrl + '/admin/queryCost',//请求后台的URL（*）TODO:管理员的管理，这个要改成后台返回租户信息的url,需要返回租户序号（后台表里的行号）账号，问卷次数，费用，操作里面是各种编辑按钮
+            method: 'POST',                      //请求方式（*）
+            striped: true,                      //是否显示行间隔色
+            cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+            pagination: true,                   //是否显示分页（*）
+            sortable: true,                     //是否排序
+            sortOrder: "desc",                   //排序方式 'desc'降序
+            queryParamsType: '',
+            sortName: "creation_date",
+            dataType: 'json',
+            paginationShowPageGo: true,
+            showJumpto: true,
+            pageNumber: 1, //初始化加载第一页，默认第一页
+            queryParams: queryParams,//请求服务器时所传的参数
+            sidePagination: 'server',//指定服务器端分页
+            pageSize: 10,//单页记录数
+            pageList: [10, 20, 30, 40],//分页步进值
+            search: false, //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
+            silent: true,
+            showRefresh: false,                  //是否显示刷新按钮
+            showToggle: false,
+            minimumCountColumns: 2,             //最少允许的列数
+            uniqueId: "id",                     //每一行的唯一标识，一般为主键列
+
+            columns: [{
+                checkbox: true,
+                visible: false
+            }, {
+                field: 'id',
+                title: '序号',
+                align: 'center',
+                formatter: function (value, row, index) {
+                    return index + 1;
+                }
+            },
+                {
+                    field: 'username',
+                    title: '用户账号',
+                    align: 'center',
+                    width: '230px'
+                },
+                {
+                    field: 'useTime',
+                    title: '答卷情况',
+                    align: 'center'
+                },
+                {
+                    field: 'operation',
+                    title: '操作',
+                    align: 'center',
+                    events: operateEvents,//给按钮注册事件
+                    formatter: addFunctionAlty//表格中增加按钮
+                }],
+
+            responseHandler: function (res) {
+                //console.log(res);
+                if (res.code == "666") {
+                    var userInfo = res.data.list;
+                    console.log(userInfo);
+                    //var userInfo=JSON.parse(res.data)
+                    // console.log(userInfo)
+                    //var userInfo=JSON.parse('[{"password":"1","startTime":"2022-05-12T10:09:28","id":"1","endTime":"2022-05-12T10:09:30","username":"aa","status":"1"},{"password":"123","startTime":"2022-05-12T12:10:37","id":"290e08f3ea154e33ad56a18171642db1","endTime":"2022-06-11T12:10:37","username":"aaa","status":"1"},{"password":"1","startTime":"2018-10-24T09:49:00","id":"8ceeee2995f3459ba1955f85245dc7a5","endTime":"2025-11-24T09:49:00","username":"admin","status":"1"},{"password":"aa","startTime":"2022-05-16T12:01:54","id":"a6f15c3be07f42e5965bec199f7ebbe6","endTime":"2022-06-15T12:01:54","username":"aaaaa","status":"1"}]');
+                    var NewData = [];
+                    if (userInfo.length) {
+                        for (var i = 0; i < userInfo.length; i++) {
+                            var dataNewObj = {
+                                'id': '',
+                                "username": '',
+                                'useTime': '',
+                                'totalCost': ''
+                            };
+                            dataNewObj.id = userInfo[i].id;
+                            dataNewObj.username = userInfo[i].username;
+                            dataNewObj.useTime = userInfo[i].useTime;
+                            dataNewObj.totalCost = userInfo[i].totalCost;
+                            NewData.push(dataNewObj);
+                        }
+                        console.log(NewData)
+                    }
+                    var data = {
+                        total: res.data.total,//改动
+                        rows: NewData
+                    };
+                    return data;
+                }
+
+            }
+        });
+    };
+
+    // 得到查询的参数
+    function queryParams(params) {
+        var username = $("#keyWord").val();
+        //console.log(userName);
+        var temp = {//这里的键的名字和控制器的变量名必须一致，这边改动，控制器也需要改成一样的
+            // rows: params.limit,//页面大小
+            pageNum: params.pageNumber,
+            pageSize: params.pageSize,
+            // create_time
+            // page: (params.offset / params.limit) + 1,
+            username: username,
+            sortName: params.sortName,
+            sortOrder: params.sortOrder //排序方式（desc，asc）
+        };
+        return JSON.stringify(temp);
+    }
+
+    return oTableInit;
+}
+
+
+window.operateEvents = {
+    //编辑
+    'click #btn_count': function (e, value, row, index) {
+        id = row.id;
+        $.cookie('questionId', id);
+    }
+};
+
+
+// 表格中按钮
+function addFunctionAlty(value, row, index) {
+    var btnText = '';
+
+    btnText += "<button type=\"button\" id=\"btn_look\" onclick=\"resetPassword(" + "'" + row.id + "'" + ")\" style='width: 77px;' class=\"btn btn-default-g ajax-link\">答题情况</button>&nbsp;&nbsp;";
+
+    btnText += "<button type=\"button\" id=\"btn_look\" onclick=\"editUserPage(" + "'" + row.id + "')\" class=\"btn btn-default-g ajax-link\">编辑</button>&nbsp;&nbsp;";
+
+    btnText += "<button type=\"button\" id=\"btn_stop" + row.id + "\" onclick=\"deleteUser(" + "'" + row.id + "'" + ")\" class=\"btn btn-danger-g ajax-link\">删除</button>&nbsp;&nbsp;";
+
+    return btnText;
+}
+
+// 打开创建用户页
+function openCreateUserPage(id, value) {
+
+    deleteCookie("userTitle");
+    setCookie("userTitle", value);
+    if (id != '') {
+        deleteCookie("userId");
+        setCookie("userId", id);
+    }
+    //打开url窗口文件
+    window.location.href = 'createNewRespondent.html';
+}
+
+function editUserPage() {
+
+    alert("编辑答者")
+}
+
+// 修改用户状态（禁用、开启）
+function changeStatus(index) {
+
+    alert("修改答者状态")
+}
+
+//删除用户
+function deleteUser(id) {
+
+    alert("删除答者")
+}
+
